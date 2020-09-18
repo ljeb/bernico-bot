e7e5920b5e88504e8c4eb4df70b5a72843de9987
ljeb <jebarali@msu.edu>
2020-09-18 17:26:53 -0400

    cleans up code, removes tweepy functionality

--- bot.py
+++ bot.py
@@ -1,56 +1,43 @@
-import tweepy
 import json
 import config
-
-from tweepy.streaming import StreamListener
-from tweepy import Stream
-from tweepy import OAuthHandler
-from tweepy import API
 from twython import Twython
+from twython import TwythonStreamer
 
 C_KEY = config.C_KEY
 C_SECRET = config.C_SECRET
 A_TOKEN_KEY = config.A_TOKEN_KEY
 A_TOKEN_SECRET = config.A_TOKEN_SECRET
 
-auth = tweepy.OAuthHandler(C_KEY, C_SECRET)
-auth.set_access_token(A_TOKEN_KEY, A_TOKEN_SECRET)
-twitter = Twython(C_KEY, C_SECRET, A_TOKEN_KEY, A_TOKEN_SECRET)
 
-
-class MyListener(StreamListener):
-
-    def __init__(self):
-        super(MyListener, self).__init__()
-
-    def on_data(self, data):
-        tweet = json.loads(data)
+class Streamer(TwythonStreamer):
+    # starts when @nicolette_sara sends tweet
+    def on_success(self, tweet):
         print("found tweet")
         if 'text' in tweet.keys() \
             and "retweeted_status" not in tweet.keys() \
                 and not tweet["in_reply_to_status_id"] \
-                and tweet["user"]["screen_name"] == "bernicobot":
+                and tweet["user"]["screen_name"] == "nicolette_sara":
             print("text:", tweet["text"])
             reply(tweet)
 
-    def on_error(self, status):
-        print(status)
+    def on_error(self, status_code):
+        print(status_code)
 
 
 def reply(tweet):
-    print("replying to tweet")
+    print("starting tweet reply")
+    twitter = Twython(C_KEY, C_SECRET, A_TOKEN_KEY, A_TOKEN_SECRET)
     video = open('women.mp4', 'rb')
     response = twitter.upload_video(media=video, media_type='video/mp4')
-    twitter.update_status(status='@bernicobot',
+    twitter.update_status(status='@nicolette_sara',
                           in_reply_to_status_id=tweet["id"],
                           media_ids=[response['media_id']])
-    print("done replying to tweet")
+    print("finished tweet reply")
 
 
 def stream():
-    myStream = Stream(auth, MyListener())
-    myStream.filter(follow=["1306981968618422274"])
-    # myStream.filter(follow=["780603996964651009"])
+    stream = Streamer(C_KEY, C_SECRET, A_TOKEN_KEY, A_TOKEN_SECRET)
+    stream.statuses.filter(follow=["780603996964651009"])
 
 
 stream()
